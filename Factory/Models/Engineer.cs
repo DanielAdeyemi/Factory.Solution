b2901a62ea78cfce6b8d6ac7fe9869d9b98f2696
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-19 08:56:04 -0700

    create Engineer model

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+    public Engineer()
+    {
+      JoinEntities = new HashSet<EngineerMachine>();
+    }
+  }
+}
